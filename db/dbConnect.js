406607466e302de201bdd0bc7c0b1539620ff8ae
twtwkim <alfog23@gmail.com>
2024-11-29 21:31:17 +0900

    modify MONGODB_URI in dbConnect.js

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = `${process.env.MONGODB_URI}`;
+const MONGODB_URI = `${process.env.MONGODB_URI}/first-api`;
+console.log(MONGODB_URI);
 
 if (!MONGODB_URI) {
   throw new Error(
